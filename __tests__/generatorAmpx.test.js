02e079098018186f2e6b7c0debceb49283b4715a
joelchu <joelckchu@gmail.com>
2018-04-14 13:29:15 +0800

    remove unused test file

--- __tests__/generatorAmpx.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-// Const generatorAmpx = require('../index.js');
-
-describe('generatorAmpx', () => {
-  it('has a test', () => {
-    assert(false, 'generatorAmpx should have a test');
-  });
-});
